542086272ea9546900ae95c1d4247f3873c8ae13
Matiboux <matiboux@gmail.com>
2025-06-22 20:57:51 +0200

    Update parse_all.sh

--- parse_all.sh
+++ parse_all.sh
@@ -1,19 +1,48 @@
 #!/bin/sh
 
+RESET_DOMAINS=0
+if [ $# -gt 0 ] && ([ "$1" = '-r' ] || [ "$1" = '--reset-domains' ]); then
+	RESET_DOMAINS=1
+fi
+
+if [ $RESET_DOMAINS -eq 1 ]; then
+	rm -rf domains/
+fi
+
+echo ''
+
 # InternetBS
+echo 'Parsing InternetBS domain prices...'
 if [ -f 'internetbs.html' ]; then
 	python parse_internetbs.py internetbs.html
 fi
+if [ -f 'internetbs_eur.html' ]; then
+	python parse_internetbs.py internetbs_eur.html
+fi
+if [ -f 'internetbs_usd.html' ]; then
+	python parse_internetbs.py internetbs_usd.html
+fi
+echo ''
 
 # Scaleway
+echo 'Parsing Scaleway domain prices...'
 if [ -f 'scaleway.html' ]; then
 	python parse_scaleway.py scaleway.html
 fi
+echo ''
 
 # Namecheap
+echo 'Parsing Namecheap domain prices...'
 if [ -f 'namecheap.html' ]; then
 	python parse_namecheap.py namecheap.html
 fi
+if [ -f 'namecheap_eur.html' ]; then
+	python parse_namecheap.py namecheap_eur.html
+fi
+if [ -f 'namecheap_usd.html' ]; then
+	python parse_namecheap.py namecheap_usd.html
+fi
+echo ''
 
 # Gandi
 if [ -f 'gandi.html' ]; then
@@ -21,6 +50,7 @@ if [ -f 'gandi.html' ]; then
 fi
 
 # OVH
+echo 'Parsing OVH domain prices...'
 if [ -f 'ovh.html' ]; then
 	python parse_ovh.py ovh.html
 fi
@@ -30,3 +60,4 @@ fi
 if [ -f 'ovh_en.html' ]; then
 	python parse_ovh.py ovh_en.html
 fi
+echo ''
