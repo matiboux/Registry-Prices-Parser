96c46865635ebadba6fa9627623a2070aaae9833
Matiboux <matiboux@gmail.com>
2025-06-22 21:56:52 +0200

    Update parse_all.sh

--- parse_all.sh
+++ parse_all.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Requirements:
+# apt update && apt install -y wget; pip install beautifulsoup4
+
 RESET_DOMAINS=0
 if [ $# -gt 0 ] && ([ "$1" = '-r' ] || [ "$1" = '--reset-domains' ]); then
 	RESET_DOMAINS=1
@@ -9,55 +12,84 @@ if [ $RESET_DOMAINS -eq 1 ]; then
 	rm -rf domains/
 fi
 
+mkdir -p html/
+mkdir -p domains/
+
 echo ''
 
-# InternetBS
+# -- InternetBS --
+# Automatic download is not available
 echo 'Parsing InternetBS domain prices...'
-if [ -f 'internetbs.html' ]; then
-	python parse_internetbs.py internetbs.html
+if [ -f 'html/internetbs.html' ]; then
+	python parse_internetbs.py html/internetbs.html
 fi
-if [ -f 'internetbs_eur.html' ]; then
-	python parse_internetbs.py internetbs_eur.html
+if [ -f 'html/internetbs_eur.html' ]; then
+	python parse_internetbs.py html/internetbs_eur.html
 fi
-if [ -f 'internetbs_usd.html' ]; then
-	python parse_internetbs.py internetbs_usd.html
+if [ -f 'html/internetbs_usd.html' ]; then
+	python parse_internetbs.py html/internetbs_usd.html
 fi
 echo ''
 
-# Scaleway
+# -- Scaleway --
 echo 'Parsing Scaleway domain prices...'
-if [ -f 'scaleway.html' ]; then
-	python parse_scaleway.py scaleway.html
+if [ ! -f 'html/scaleway.html' ]; then
+	wget -q -O 'html/scaleway.html' 'https://www.scaleway.com/en/domains-name/' 2>/dev/null
+fi
+if [ -f 'html/scaleway.html' ]; then
+	python parse_scaleway.py html/scaleway.html
 fi
 echo ''
 
-# Namecheap
+# -- Namecheap --
+# Automatic download is not available
+# https://www.namecheap.com/domains/
+# Select "Choose from ALL" and copy source code instead of downloading
 echo 'Parsing Namecheap domain prices...'
-if [ -f 'namecheap.html' ]; then
-	python parse_namecheap.py namecheap.html
+if [ -f 'html/namecheap.html' ]; then
+	python parse_namecheap.py html/namecheap.html
 fi
-if [ -f 'namecheap_eur.html' ]; then
-	python parse_namecheap.py namecheap_eur.html
+if [ -f 'html/namecheap_eur.html' ]; then
+	python parse_namecheap.py html/namecheap_eur.html
 fi
-if [ -f 'namecheap_usd.html' ]; then
-	python parse_namecheap.py namecheap_usd.html
+if [ -f 'html/namecheap_usd.html' ]; then
+	python parse_namecheap.py html/namecheap_usd.html
 fi
 echo ''
 
-# Gandi
-if [ -f 'gandi.html' ]; then
-	python parse_gandi.py gandi.html
+# -- Gandi --
+echo 'Parsing Gandi domain prices...'
+if [ -f 'html/gandi.html' ]; then
+	python parse_gandi.py html/gandi.html
 fi
+wget -q -O 'html/gandi_xn.html' 'https://www.gandi.net/en/domain/tld?prefix=xn--' 2>/dev/null
+if [ -f 'html/gandi_xn.html' ]; then
+	python parse_gandi.py html/gandi_xn.html
+fi
+while read -r letter; do
+	if [ ! -f "html/gandi_${letter}.html" ]; then
+		wget -q -O "html/gandi_${letter}.html" "https://www.gandi.net/en/domain/tld?prefix=${letter}" 2>/dev/null
+	fi
+	if [ -f "html/gandi_${letter}.html" ]; then
+		python parse_gandi.py "html/gandi_${letter}.html"
+	fi
+done <<EOF
+$(echo 'abcdefghijklmnopqrstuvwxyz' | fold -w 1)
+EOF
+echo ''
 
-# OVH
+# -- OVH --
+# Automatic download is not available
+# https://www.ovhcloud.com/en/domains/tld/
+# Toggle "Renewal" and "Transfer" checkboxes
 echo 'Parsing OVH domain prices...'
-if [ -f 'ovh.html' ]; then
-	python parse_ovh.py ovh.html
+if [ -f 'html/ovh.html' ]; then
+	python parse_ovh.py html/ovh.html
 fi
-if [ -f 'ovh_fr.html' ]; then
-	python parse_ovh.py ovh_fr.html
+if [ -f 'html/ovh_fr.html' ]; then
+	python parse_ovh.py html/ovh_fr.html
 fi
-if [ -f 'ovh_en.html' ]; then
-	python parse_ovh.py ovh_en.html
+if [ -f 'html/ovh_en.html' ]; then
+	python parse_ovh.py html/ovh_en.html
 fi
 echo ''
