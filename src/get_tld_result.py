560ec3d8df34cef1a3602107be5717fae9001fbf
Matiboux <matiboux@gmail.com>
2025-06-23 15:25:10 +0200

    Fix get_tld_result

--- src/get_tld_result.py
+++ src/get_tld_result.py
@@ -8,27 +8,27 @@ def get_tld_result(
 ):
 	return {
 		**(
-			{ 'registration': { (registration['currency'] or ''): registration['price'] } }
+			{ 'registration': { (registration['currency'] or 'unknown'): registration['price'] } }
 			if registration else
 			{}
 		),
 		**(
-			{ 'renewal': { (renewal['currency'] or ''): renewal['price'] } }
+			{ 'renewal': { (renewal['currency'] or 'unknown'): renewal['price'] } }
 			if renewal else
 			{}
 		),
 		**(
-			{ 'transfer': { (transfer['currency'] or ''): transfer['price'] } }
+			{ 'transfer': { (transfer['currency'] or 'unknown'): transfer['price'] } }
 			if transfer else
 			{}
 		),
 		**(
-			{ 'owner_change': { (owner_change['currency'] or ''): owner_change['price'] } }
+			{ 'owner_change': { (owner_change['currency'] or 'unknown'): owner_change['price'] } }
 			if owner_change else
 			{}
 		),
 		**(
-			{ 'restore': { (restore['currency'] or ''): restore['price'] } }
+			{ 'restore': { (restore['currency'] or 'unknown'): restore['price'] } }
 			if restore else
 			{}
 		),
